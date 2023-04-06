c8ad0128a992af32ac1fa14999b908fd69fb70cf
Anandmohanty <mohantyanand3@gmail.com>
2023-04-06 13:52:30 +0530

    [Anand] - Encapsulation

--- Program.cs
+++ Program.cs
@@ -40,6 +40,14 @@
                     car.MethodInVehical();
                     car.MethodeInCar();
                     break;
+
+                case 4:
+                    Encapsulation encapsulation = new Encapsulation();
+                    Console.WriteLine("Enter Employee Name");
+                    encapsulation.name = Console.ReadLine();
+                    encapsulation.SetSalary(8000);
+                    Console.WriteLine("Salary of" + encapsulation.name + " is " + encapsulation.GetSalary());
+                    break;
             }
         }
     }
--- inheritance.cs
+++ inheritance.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace OppsConcept
 {
-    internal class inheritance
+    public class Inheritance // base class (parent) 
     {
+        public void Brand(string brand)
+        {
+            Console.WriteLine(brand);
+        }
+
+    }
+    public class Vehicle : Inheritance // derived class (child)
+    {
+        public void MethodInVehical()
+        {
+            Console.WriteLine("This is Method in Vehical Class");
+        }
+    }
+
+    public class Car : Vehicle // derived class (child)
+    {
+        public void MethodeInCar()
+        {
+            Console.WriteLine("This is Method in Car Class");
+        }
     }
 }
