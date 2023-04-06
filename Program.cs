eec8247cc54958d716d4c70776c15ffce7b06e2d
Anandmohanty <mohantyanand3@gmail.com>
2023-04-06 13:06:59 +0530

    [Anand - Oject And Class

--- Program.cs
+++ Program.cs
@@ -5,5 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To OOP Concept");
+            Console.WriteLine("Select option For Respective Concepts \n 1.ObjectAndClass \n 2.Polymorphism \n 3.Inheritance \n 4.Encapsulation \n 5.Abstaction \n 6.Variable \n 7. Type of Method \n 8. Value and Referance Type \n 9.Type of Inheritance");
+            int result = Convert.ToInt32(Console.ReadLine());
+            switch (result)
+            {
+                case 1:
+                    ObjectAndClass objectAndClass = new ObjectAndClass();
+                    objectAndClass.SampleMethode();
+                    break;
+            }
         }
     }
+}
